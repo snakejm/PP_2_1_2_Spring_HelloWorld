d7166590da0341aae1ffd96efead859eabd6c969
snakejm <snakejedymaster@gmail.com>
2022-10-20 20:35:25 +0300

    create cat bean prototype

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("helloWorld1 == helloWorld2? " + (helloWorld1 == helloWorld2));
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("cat1 == cat2? " + (cat1 == cat2));
     }
 }
